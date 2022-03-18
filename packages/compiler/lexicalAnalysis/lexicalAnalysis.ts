24ab0a59e21cc1180af8fdce69b11954cd74e832
Joao-Arthur <joao.lothamer@gmail.com>
2022-03-17 22:09:03 -0300

    add comment to start logic

--- packages/compiler/lexicalAnalysis/lexicalAnalysis.ts
+++ packages/compiler/lexicalAnalysis/lexicalAnalysis.ts
@@ -11,25 +11,58 @@ export function lexicalAnalysis(sourceCode: string): lexicalTokens[] {
     let tokens: lexicalTokens[] = [];
     let i = 0;
 
-    let currentFoundToken: any = null;
-    let possibleSymbols: any[] = [];
+    let current: {
+        type: 'numeric' | 'identifier' | 'comment' | 'string';
+        symbol: string;
+    } | null = null;
+
+
+    /*
+
+    existe os seguintes grupos de itens a serem considerados
+
+comentários
+strings
+-> dentro deles, tudo é ignorado
+-> se chegar no final e não tiver fechamento??
+
+depois, tem símbolos, que quando encontrados, dão match automaticamente
+(, ), >=, > (cuidado, nesse caso tem que verificar o seguinte :( ), ponto,virgula.
+
+então, tem números, começa e termina com números, se tiver um dígito, aí dá erro, eu acho
+
+depois tem keywords, que só dão match na hora de adiciona à lista
+senão, é um identificador comum
+
+todo o resto é ignorado
+
+*/
+
     for (i = 0; i < sourceCode.length; i++) {
         const character = sourceCode[i].toLocaleLowerCase();
 
-        if (!currentFoundToken) {
-            if (!possibleSymbols.length) {
-                possibleSymbols = symbols.filter(({ symbol }) =>
-                    symbol.startsWith(character)
-                );
-
-                if (possibleSymbols.length > 1) {
-                } else if (possibleSymbols.length === 1) {
-                    const [onlyPossibleSymbol] = possibleSymbols;
-                    onlyPossibleSymbol.symbol;
+        if (current) {
+            if (current.type === 'numeric')
+                if (numbers.includes(character)) {
+                    current.symbol += character;
+                } else {
+                    //error?
                 }
-            } else {
-                possibleSymbols = possibleSymbols.filter();
+            if (current.type === 'identifier') {
+                if (
+                    letters.includes(character) ||
+                    numbers.includes(character)
+                ) {
+                    current.symbol += character;
+                } else {
+                    if()
+                }
             }
+        } else {
+            if (letters.includes(character))
+                current = { type: 'identifier', symbol: character };
+            if (numbers.includes(character))
+                current = { type: 'numeric', symbol: character };
         }
     }
     return tokens;
