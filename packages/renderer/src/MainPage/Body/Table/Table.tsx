913320af1a2ec269ba8363b0b9aefeac14c9ef9d
Fernando R <fernandorodriguesdev@outlook.com>
2022-03-18 20:01:08 -0300

    ajuste table

--- packages/renderer/src/MainPage/Body/Table/Table.tsx
+++ packages/renderer/src/MainPage/Body/Table/Table.tsx
@@ -9,14 +9,14 @@ export function Table({ data }: props) {
     return (
         <div className='w-1/2 text-left overflow-auto text-white bg-primary '>
             <table className='w-full '>
-                <tr className=''>
+                <tr className='bg-black/20'>
                     <th className='px-4'>Identificador</th>
                     <th className='px-4'>Token</th>
                 </tr>
                 {data.map(({ id, token }) => (
-                    <tr>
-                        <td className='px-4'>{id}</td>
-                        <td className='px-4'>{token}</td>
+                    <tr className='border-b border-white/10'>
+                        <td className='px-4 py-2'>{id}</td>
+                        <td className='px-4 py-2'>{token}</td>
                     </tr>
                 ))}
             </table>
