c94411c3045f5e0b388da4c6c0a9afb1cf42dcbe
joao-arthur <joao.lothamer@gmail.com>
2022-07-07 21:15:41 -0300

    test: semantic analysis

--- packages/compiler/semanticalAnalysis/semanticAnalysis.test.ts
+++ packages/compiler/semanticalAnalysis/semanticAnalysis.test.ts
@@ -53,6 +53,40 @@ describe('semanticAnalysis', () => {
                 { line: 3, word: '=', id: symbols['='] },
                 { line: 3, word: '31415', id: symbols.inteiro },
                 { line: 3, word: ';', id: symbols[';'] },
+                { line: 4, word: 'pi', id: symbols.identificador },
+                { line: 4, word: '=', id: symbols['='] },
+                { line: 4, word: '1234', id: symbols.inteiro },
+                { line: 4, word: ';', id: symbols[';'] },
+                { line: 5, word: 'var', id: symbols.var },
+                { line: 6, word: 'cur', id: symbols.identificador },
+                { line: 6, word: ':', id: symbols[':'] },
+                { line: 6, word: 'integer', id: symbols.integer },
+                { line: 6, word: ';', id: symbols[';'] },
+                { line: 7, word: 'currentvalue', id: symbols.identificador },
+                { line: 7, word: ':', id: symbols[':'] },
+                { line: 7, word: 'integer', id: symbols.integer },
+                { line: 7, word: ';', id: symbols[';'] },
+                { line: 1, word: 'begin', id: symbols.var },
+                { line: 1, word: 'end', id: symbols.var }
+            ])
+        ).toThrow(VariableAlreadyDeclaredException);
+    });
+
+    it('should verify duplicated const and var', () => {
+        expect(() =>
+            semanticAnalysis([
+                { line: 1, word: 'program', id: symbols.var },
+                {
+                    line: 1,
+                    word: 'DuplicatedVariablesProgram',
+                    id: symbols.program
+                },
+                { line: 1, word: ';', id: symbols[';'] },
+                { line: 2, word: 'const', id: symbols.const },
+                { line: 3, word: 'pi', id: symbols.identificador },
+                { line: 3, word: '=', id: symbols['='] },
+                { line: 3, word: '31415', id: symbols.inteiro },
+                { line: 3, word: ';', id: symbols[';'] },
                 { line: 4, word: 'e', id: symbols.identificador },
                 { line: 4, word: '=', id: symbols['='] },
                 { line: 4, word: '1234', id: symbols.inteiro },
@@ -103,7 +137,7 @@ describe('semanticAnalysis', () => {
                 { line: 6, word: ';', id: symbols[';'] },
                 { line: 1, word: 'end', id: symbols.var }
             ])
-        ).toThrow(VariableNotInScopeException);
+        ).not.toThrow();
 
         expect(() =>
             semanticAnalysis([
@@ -135,7 +169,7 @@ describe('semanticAnalysis', () => {
                 { line: 6, word: ';', id: symbols[';'] },
                 { line: 6, word: 'cur', id: symbols.identificador },
                 { line: 6, word: ':=', id: symbols[':='] },
-                { line: 6, word: '4', id: symbols.inteiro },
+                { line: 6, word: 'oldvalue', id: symbols.identificador },
                 { line: 6, word: ';', id: symbols[';'] },
                 { line: 1, word: 'end', id: symbols.var }
             ])
@@ -161,6 +195,22 @@ describe('semanticAnalysis', () => {
                 { line: 4, word: '=', id: symbols['='] },
                 { line: 4, word: '1234', id: symbols.inteiro },
                 { line: 4, word: ';', id: symbols[';'] },
+                { line: 5, word: 'procedure', id: symbols.procedure },
+                { line: 5, word: 'printvalue', id: symbols.identificador },
+                { line: 5, word: ';', id: symbols[';'] },
+                { line: 5, word: 'const', id: symbols.const },
+                { line: 5, word: 'pi', id: symbols.identificador },
+                { line: 5, word: '=', id: symbols['='] },
+                { line: 5, word: '298789', id: symbols.inteiro },
+                { line: 5, word: ';', id: symbols[';'] },
+                { line: 5, word: 'var', id: symbols.var },
+                { line: 5, word: 'currentvalue', id: symbols.identificador },
+                { line: 5, word: ':', id: symbols[':'] },
+                { line: 5, word: 'integer', id: symbols.integer },
+                { line: 5, word: ';', id: symbols[';'] },
+                { line: 5, word: 'begin', id: symbols.begin },
+                { line: 5, word: 'end', id: symbols.end },
+                { line: 5, word: ';', id: symbols[';'] },
                 { line: 5, word: 'var', id: symbols.var },
                 { line: 6, word: 'currentvalue', id: symbols.identificador },
                 { line: 6, word: ':', id: symbols[':'] },
@@ -173,6 +223,6 @@ describe('semanticAnalysis', () => {
                 { line: 8, word: 'begin', id: symbols.var },
                 { line: 8, word: 'end', id: symbols.var }
             ])
-        ).not.toThrow(VariableAlreadyDeclaredException);
+        ).not.toThrow();
     });
 });
